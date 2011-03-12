988dbda84a210388b5ac7ac850b63e28d488d581
Damien White <damien.white@visoftinc.com>
2011-03-12 14:36:41 -0500

    Removed actual DBs so git wouldn't track them, altered cucumber setup

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,3 +2,11 @@ require 'lib/ruby_odata'
 require 'machinist/object'
 require 'sham'
 require 'faker'
+
+require 'ftools'
+root_dir = File.expand_path(File.join(File.dirname(__FILE__), "../..", "test/SampleService/App_Data"))
+
+if !File.exists?("#{root_dir}/TestDB.mdf")
+  File.copy("#{root_dir}/_TestDB.mdf", "#{root_dir}/TestDB.mdf")
+  File.copy("#{root_dir}/_TestDB_Log.ldf", "#{root_dir}/TestDB_Log.ldf")
+end
