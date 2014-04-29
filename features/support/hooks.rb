93200aef6fe87cbd1add41a8421e67a4202a88de
Damien White <damien.white@visoftinc.com>
2014-04-28 21:21:41 -0400

    Re-recorded VCR cassette tapes with Faraday

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,5 +1,5 @@
 Before do
   VCR.use_cassette("clean_database_for_testing") do
-    RestClient.post "http://#{WEBSERVER}:#{HTTP_PORT_NUMBER}/SampleService/RubyOData.svc/CleanDatabaseForTesting", {}
+    Faraday.post "http://#{WEBSERVER}:#{HTTP_PORT_NUMBER}/SampleService/RubyOData.svc/CleanDatabaseForTesting", {}
   end
 end
