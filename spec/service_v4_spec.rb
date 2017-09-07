6564668d4e6bde19849902321ad7a0a5b49928a7
Klaus <kd.gundermann@zwick-edelstahl.de>
2017-09-07 23:00:21 +0200

    simplify url in spec

--- spec/service_v4_spec.rb
+++ spec/service_v4_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "V4 Service" do
   before(:all) do
-    stub_request(:get, /http:\/\/test\.com\/test\.svc\/\$metadata(?:\?.+)?/).
+    stub_request(:get, "http://test.com/test.svc/$metadata").
     with(:headers => DEFAULT_HEADERS).
     to_return(:status => 200, :body => File.new(File.expand_path("../fixtures/v4/edmx_metadata.xml", __FILE__)), :headers => {})
 
