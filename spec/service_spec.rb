b720b4796945abc9b8088852fa98eb95b8f4bab0
Klaus <kd.gundermann@zwick-edelstahl.de>
2017-09-07 21:40:14 +0200

    Extract to sap_spec

--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -161,25 +161,6 @@ module OData
       end
     end
 
-    describe "handling of SAP results" do
-      before(:each) do
-        # Required for the build_classes method
-        stub_request(:get, "http://test.com/test.svc/$metadata").
-          with(:headers => DEFAULT_HEADERS).
-          to_return(:status => 200, :body => File.new(File.expand_path("../fixtures/sap/edmx_sap_demo_flight.xml", __FILE__)), :headers => {})
-
-        stub_request(:get, "http://test.com/test.svc/z_demo_flightCollection").
-          with(:headers => DEFAULT_HEADERS).
-          to_return(:status => 200, :body => File.new(File.expand_path("../fixtures/sap/result_sap_demo_flight_missing_category.xml", __FILE__)), :headers => {})
-      end
-
-      it "should handle entities without a category element" do
-        svc = OData::Service.new "http://test.com/test.svc/"
-        svc.z_demo_flightCollection
-        results = svc.execute
-        results.first.should be_a_kind_of(ZDemoFlight)
-      end
-    end
 
     describe "collections, objects, metadata etc" do
       before(:each) do
---
+++ spec/services/sap_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+module OData
+
+    describe "handling of SAP results" do
+      before(:each) do
+        # Required for the build_classes method
+        stub_request(:get, "http://test.com/test.svc/$metadata").
+          with(:headers => DEFAULT_HEADERS).
+          to_return(:status => 200, :body => File.new( FIXTURES + "/sap/edmx_sap_demo_flight.xml"), :headers => {})
+
+        stub_request(:get, "http://test.com/test.svc/z_demo_flightCollection").
+          with(:headers => DEFAULT_HEADERS).
+          to_return(:status => 200, :body => File.new( FIXTURES + "/sap/result_sap_demo_flight_missing_category.xml"), :headers => {})
+      end
+
+      it "should handle entities without a category element" do
+        svc = OData::Service.new "http://test.com/test.svc/"
+        svc.z_demo_flightCollection
+        results = svc.execute
+        results.first.should be_a_kind_of(ZDemoFlight)
+      end
+    end
+
+end
