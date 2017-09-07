0520f49f8339ad89352230ccb15b3c7a1ffd2d11
Klaus <kd.gundermann@zwick-edelstahl.de>
2017-09-07 22:55:56 +0200

    define constant to path for Fixtures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,16 @@ require 'ruby_odata'
 require 'webmock/rspec'
 require 'simplecov'
 require 'rspec/its'
+
 # require 'coveralls'
 # Coveralls.wear_merged!
 
 Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
 
+FIXTURES = File.expand_path('../fixtures', __FILE__)
+
 WebMock.disable_net_connect!(allow_localhost: true)
+
 DEFAULT_HEADERS = {
   'Accept'=>'*/*; q=0.5, application/xml',
   'Accept-Encoding'=>'gzip,deflate'
