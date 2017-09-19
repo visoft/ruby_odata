5d48613ddaca3c06df0650bf00ee89d5ea4e84d9
Klaus <kd.gundermann@zwick-edelstahl.de>
2017-09-19 23:14:44 +0200

    rewrite json_serialization_spec to use subject

--- spec/internal/json_serialization_spec.rb
+++ spec/internal/json_serialization_spec.rb
@@ -14,10 +14,14 @@ module OData
 	    stub_request(:get, "http://test.com/test.svc/VirtualMachines").
 	      with(:headers => DEFAULT_HEADERS).
 	      to_return(:status => 200, :body => File.new( FIXTURES + "/ms_system_center/virtual_machines.xml"), :headers => {})
+
 	    @service = OData::Service.new "http://test.com/test.svc/", { :username => username, :password => password, :verify_ssl => false, :namespace => "VMM" }
+	  end
+
+	  subject do
 	    @service.VirtualMachines
 	    results = @service.execute
-	    @json = results.first.as_json
+	    results.first.as_json
 	  end
 
 	  after(:each) do
@@ -25,18 +29,18 @@ module OData
 		end
 
 	  it "Should quote Edm.Int64 properties" do
-	    @json["PerfDiskBytesWrite"].should be_a(String)
+	    subject["PerfDiskBytesWrite"].should be_a(String)
 	  end
 
 	  it "Should output collections with metadata" do
-	    @json["VMNetworkAssignments"].should be_a(Hash)
-	    @json["VMNetworkAssignments"].should have_key("__metadata")
-	    @json["VMNetworkAssignments"]["__metadata"].should be_a(Hash)
-	    @json["VMNetworkAssignments"]["__metadata"].should have_key("type")
-	    @json["VMNetworkAssignments"]["__metadata"]["type"].should eq("Collection(VMM.VMNetworkAssignment)")
-	    @json["VMNetworkAssignments"].should have_key("results")
-	    @json["VMNetworkAssignments"]["results"].should be_a(Array)
-	    @json["VMNetworkAssignments"]["results"].should eq([])
+	    subject["VMNetworkAssignments"].should be_a(Hash)
+	    subject["VMNetworkAssignments"].should have_key("__metadata")
+	    subject["VMNetworkAssignments"]["__metadata"].should be_a(Hash)
+	    subject["VMNetworkAssignments"]["__metadata"].should have_key("type")
+	    subject["VMNetworkAssignments"]["__metadata"]["type"].should eq("Collection(VMM.VMNetworkAssignment)")
+	    subject["VMNetworkAssignments"].should have_key("results")
+	    subject["VMNetworkAssignments"]["results"].should be_a(Array)
+	    subject["VMNetworkAssignments"]["results"].should eq([])
 	  end
 	end	
 end
