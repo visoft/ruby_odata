a345c8b638bbbb21855543eae5291488850be0ac
klaus <kd.gundermann@zwick-edelstahl.de>
2017-10-09 18:05:51 +0200

    extract spec to internal/parse_value_spec

--- spec/service_detailed_spec.rb
+++ spec/service_detailed_spec.rb
@@ -495,7 +495,7 @@ module OData
           stub_request(:get, "http://test.com/test.svc/Products?$select=Name,Price").
             with(:headers => DEFAULT_HEADERS).
             to_return(:status => 200, :body => Fixtures.load("/sample_service/result_select_products_name_price.xml"), :headers => {})
-        end        
+        end
 
         before(:each) do
           @service =OData::Service.new "http://test.com/test.svc/"
@@ -504,8 +504,8 @@ module OData
         end
 
         it "returns an Array og Products" do
-          expect(@result).to  be_an Array 
-          expect(@result).not_to be_empty 
+          expect(@result).to  be_an Array
+          expect(@result).not_to be_empty
           expect(@result.first).to be_a Product
         end
       end
@@ -552,13 +552,13 @@ module OData
         end
 
         it "retursn Categoris" do
-          @service =OData::Service.new "http://test.com/test.svc/"          
+          @service =OData::Service.new "http://test.com/test.svc/"
           @service.Categories
           c = @service.execute
         end
 
         it "doesn't error" do
-          @service =OData::Service.new "http://test.com/test.svc/"          
+          @service =OData::Service.new "http://test.com/test.svc/"
           @service.Categories.select "Name", "Products/Name"
           expect { @service.execute }.to_not raise_error
         end
@@ -576,21 +576,4 @@ module OData
     end
   end
 
-  describe_private OData::Service do
-    describe "parse value" do
-      before(:each) do
-        # Required for the build_classes method
-        stub_request(:get, "http://test.com/test.svc/$metadata").
-          with(:headers => DEFAULT_HEADERS).
-          to_return(:status => 200, :body => Fixtures.load("edmx_empty.xml" ), :headers => {})
-      end
-
-      it "should not error on an 'out of range' date" do
-        # This date was returned in the Netflix OData service and failed with an ArgumentError: out of range using 1.8.7 (2010-12-23 patchlevel 330) [i386-mingw32]
-        @service =OData::Service.new "http://test.com/test.svc/"
-        element_to_parse = Nokogiri::XML.parse('<d:AvailableFrom m:type="Edm.DateTime">2100-01-01T00:00:00</d:AvailableFrom>').elements[0]
-        lambda { @service.parse_value_xml(element_to_parse) }.should_not raise_exception
-      end
-    end
-  end
 end
